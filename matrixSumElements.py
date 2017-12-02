f9f55be705c48c1c142efd6020a2553534f80675
Lakshay Kumar <33530741+ineffablelucky@users.noreply.github.com>
2017-12-03 02:33:44 +0530

    Update matrixSumElements.py

--- matrixSumElements.py
+++ matrixSumElements.py
@@ -4,16 +4,16 @@
 # because they are haunted, so all the bots are afraid of them.
 # That is why any room that is free or is located anywhere below a free room in the same column is not
 # considered suitable for the bots to live in.
-
+# Help the bots calculate the total price of all the rooms that are suitable for them.
 
 def matrixElementsSum(matrix):
     for i in range(len(matrix)-1):
         for j in range(len(matrix[i])):
             if matrix[i][j]==0:
-                matrix[i+1][j]=0
+                matrix[i+1][j]=0 #converting all values below 0 to 0
 
-    suma=0
-    for i in range(len(matrix)):
-           suma+= sum(matrix[i])
+    totprice=0
+    for i in range(len(matrix)): 
+           totprice+= sum(matrix[i]) #add the sum of all rows
 
-    return suma
+    return totprice
